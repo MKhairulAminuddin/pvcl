a0d49f0fa47fa80d6720e19f835468e5590ffaea
khaliq <khaliq2network@gmail.com>
2021-06-27 15:08:06 +0800

    refactor

---
+++ Form.sql
@@ -0,0 +1,43 @@
+﻿
+CREATE TABLE [dbo].[FormHeader](
+	[Id] INT NOT NULL Identity(1,1) PRIMARY KEY, 
+    [Title] NVARCHAR(MAX) NOT NULL, 
+
+    [FormType] NVARCHAR(150) NULL, 
+
+	[Submission] DATETIME NULL,
+
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [CreatedDate] DATETIME NULL DEFAULT GETDATE(), 
+
+    [ApprovedBy] NVARCHAR(50) NULL, 
+    [ApprovedDate] DATETIME NULL
+)
+
+CREATE TABLE [dbo].[AmsdInflowFunds](
+	[Id] INT NOT NULL Identity(1,1) PRIMARY KEY, 
+    [FormHeaderId] INT NOT NULL FOREIGN KEY REFERENCES FormHeader(Id), 
+
+	[FundType] NVARCHAR(250) NULL,
+	[Bank] NVARCHAR(150) NULL,
+	[Amount] DECIMAL(18, 2) NULL,
+
+	[LatestSubmission] DATETIME NULL,
+
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [CreatedDate] DATETIME NULL DEFAULT GETDATE(), 
+    [UpdatedBy] NVARCHAR(50) NULL, 
+    [UpdatedDate] DATETIME NULL
+)
+
+CREATE TABLE [dbo].[AmsdInflowFundsHistory](
+	[Id] INT NOT NULL Identity(1,1) PRIMARY KEY, 
+    [FormHeaderId] INT NOT NULL, 
+
+	[LatestSubmission] DATETIME NULL,
+
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [CreatedDate] DATETIME NULL DEFAULT GETDATE(), 
+    [UpdatedBy] NVARCHAR(50) NULL, 
+    [UpdatedDate] DATETIME NULL
+)
---
+++ Presentation/xDC.Web/Controllers/SettingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xDC_Web.Controllers
+{
+    [Authorize(Roles = "Administrator, Power User")]
+    public class SettingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DropdownConfig()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationConfig()
+        {
+            return View();
+        }
+
+        public ActionResult Workflow()
+        {
+            return View();
+        }
+
+        public ActionResult AuditTrail()
+        {
+            return View();
+        }
+
+    }
+}
