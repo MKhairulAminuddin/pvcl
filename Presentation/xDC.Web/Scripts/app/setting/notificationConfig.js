6724868b535f7f3d7323f94f22573a4f3049f209
khaliq <khaliq2network@gmail.com>
2021-07-07 23:25:36 +0800

    active directory dropdown list in notification config page

--- Presentation/xDC.Web/Scripts/app/setting/notificationConfig.js
+++ Presentation/xDC.Web/Scripts/app/setting/notificationConfig.js
@@ -1,1 +1,24 @@
-﻿
+﻿(function ($, window, document) {
+
+    $(function () {
+        var adUsersStore = DevExpress.data.AspNet.createStore({
+            key: "username",
+            loadUrl: "../api/common/GetActiveDirectoryUsers"
+        });
+
+        $("#contributionEmailList").dxTagBox({
+            dataSource: adUsersStore,
+            displayExpr: "displayName",
+            valueExpr: "email",
+            searchEnabled: true,
+            itemTemplate: function (data) {
+                return "<div class='active-directory-dropdown'>" +
+                    "<p class='active-directory-title'>" + data.displayName + "</p>" +
+                    "<p class='active-directory-subtitle'>" + data.title + ", " + data.department + "</p>" +
+                    "<p class='active-directory-subtitle'>" + data.email + "</p>" +
+                    "</div>";
+            }
+        }).dxTagBox("instance");
+
+    });
+}(window.jQuery, window, document));
