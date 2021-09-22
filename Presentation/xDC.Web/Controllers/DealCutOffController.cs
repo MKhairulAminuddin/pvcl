94b0f4b18d8785a617a4ca1afe53c6a517073969
khaliq <khaliq2network@gmail.com>
2021-09-22 08:21:15 +0800

    up up

---
+++ Presentation/xDC.Web/Controllers/DealCutOffController.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using xDC.Logging;
+using xDC.Utils;
+using xDC_Web.Extension.DocGenerator;
+using xDC_Web.Extension.MailMerge;
+using xDC_Web.ViewModels.Fid.DealCutOff;
+
+namespace xDC_Web.Controllers
+{
+    [Authorize(Roles = "Administrator, Power User, FID, ISSD")]
+    [RoutePrefix("DealCutOff")]
+    public class DealCutOffController : Controller
+    {
+        // GET: DealCutOff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Deal Cut Off (MYR)
+
+        public ActionResult DealCutOffMyr()
+        {
+            return View("Myr");
+        }
+
+        [HttpPost]
+        [Route("Myr/Print")]
+        public ActionResult DealCutOffMyr_Print(string TradeDate, bool isExportAsExcel)
+        {
+            try
+            {
+                DateTime? selectedDate = DateTime.Now;
+                if (!string.IsNullOrEmpty(TradeDate))
+                {
+                    selectedDate = Common.ConvertEpochToDateTime(Convert.ToInt64(TradeDate));
+                }
+
+                var generatedDocumentFile = new DealCutOffForm_MYR().GenerateFile(selectedDate.Value, isExportAsExcel);
+
+                if (!string.IsNullOrEmpty(generatedDocumentFile))
+                {
+                    return Content(generatedDocumentFile);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+                return HttpNotFound();
+            }
+        }
+
+        [Route("Myr/Printed/{id}")]
+        public ActionResult DealCutOffMyr_ViewPrinted(string id)
+        {
+            try
+            {
+                var fileStream = new DocGeneratorBase().GetFile(id);
+
+                if (fileStream != null)
+                {
+                    var fileName = Common.GetFileName(fileStream);
+                    Response.AddHeader("Content-Disposition", "attachment; filename=" + fileName);
+
+                    if (Common.GetFileExt(fileStream) == ".xlsx")
+                    {
+                        return File(fileStream, Common.ConvertIndexToContentType(4));
+                    }
+                    else
+                    {
+                        return File(fileStream, Common.ConvertIndexToContentType(11));
+                    }
+
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Generated file not found... sorry...";
+                    return View("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View("Error");
+            }
+        }
+
+        public ActionResult DealCutOffMyrPreview(string TradeDate, int SheetIndex)
+        {
+            DateTime? selectedDate = DateTime.Now;
+            if (!string.IsNullOrEmpty(TradeDate))
+            {
+                selectedDate = Common.ConvertEpochToDateTime(Convert.ToInt64(TradeDate));
+            }
+
+            var previewModel = new SpreadsheetPreviewModel
+            {
+                Workbook = new DealCutOffForm_MYR().GenerateWorkbook(selectedDate),
+                PreviewSheetIndex = SheetIndex
+            };
+            return GenerateHtmlPreview(previewModel);
+        }
+
+        protected ContentResult GenerateHtmlPreview(SpreadsheetPreviewModel previewModel)
+        {
+            ContentResult result = new ContentResult();
+            result.ContentType = "text/html";
+            result.Content = GenerateHtmlContentForWorkbook(previewModel);
+            return result;
+        }
+
+        string GenerateHtmlContentForWorkbook(SpreadsheetPreviewModel previewModel)
+        {
+            MemoryStream stream = new MemoryStream();
+            HtmlContentGenerator htmlGenerator = new HtmlContentGenerator(stream);
+            htmlGenerator.Generate(previewModel.Workbook, previewModel.PreviewSheetIndex);
+            stream.Seek(0, SeekOrigin.Begin);
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+        #endregion
+
+        #region Deal Cut Off (FCY)
+
+        public ActionResult DealCutOffFcy()
+        {
+            return View("Fcy");
+        }
+
+        [HttpPost]
+        [Route("Fcy/Print")]
+        public ActionResult DealCutOffFcy_Print(string TradeDate, bool isExportAsExcel)
+        {
+            try
+            {
+                DateTime? selectedDate = DateTime.Now;
+                if (!string.IsNullOrEmpty(TradeDate))
+                {
+                    selectedDate = Common.ConvertEpochToDateTime(Convert.ToInt64(TradeDate));
+                }
+
+                var generatedDocumentFile = new DealCutOffForm_FCY().GenerateFile(selectedDate.Value, isExportAsExcel);
+
+                if (!string.IsNullOrEmpty(generatedDocumentFile))
+                {
+                    return Content(generatedDocumentFile);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+                return HttpNotFound();
+            }
+        }
+
+        [Route("Fcy/Printed/{id}")]
+        public ActionResult DealCutOffFcy_ViewPrinted(string id)
+        {
+            try
+            {
+                var fileStream = new DocGeneratorBase().GetFile(id);
+
+                if (fileStream != null)
+                {
+                    var fileName = Common.GetFileName(fileStream);
+                    Response.AddHeader("Content-Disposition", "attachment; filename=" + fileName);
+
+                    if (Common.GetFileExt(fileStream) == ".xlsx")
+                    {
+                        return File(fileStream, Common.ConvertIndexToContentType(4));
+                    }
+                    else
+                    {
+                        return File(fileStream, Common.ConvertIndexToContentType(11));
+                    }
+
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Generated file not found... sorry...";
+                    return View("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View("Error");
+            }
+        }
+
+        public ActionResult DealCutOffFcyPreview(string TradeDate, int SheetIndex)
+        {
+            DateTime selectedDate = DateTime.Now;
+            if (!string.IsNullOrEmpty(TradeDate))
+            {
+                selectedDate = (DateTime)Common.ConvertEpochToDateTime(Convert.ToInt64(TradeDate));
+            }
+
+            var previewModel = new SpreadsheetPreviewModel
+            {
+                Workbook = new DealCutOffForm_FCY().GenerateWorkbook(selectedDate),
+                PreviewSheetIndex = SheetIndex
+            };
+            return GenerateHtmlPreview(previewModel);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Presentation/xDC.Web/Scripts/app/dealCutOff/fcy.js
@@ -0,0 +1,115 @@
+﻿(function ($, window, document) {
+    $(function () {
+        //#region Variable Definition
+
+        var $dateSelectionBtn,
+            $printBtn;
+
+        var referenceUrl = {
+            printRequest: window.location.origin + "/DealCutOff/Fcy/Print",
+            printResponse: window.location.origin + "/DealCutOff/Fcy/Printed/"
+        };
+
+        //#endregion
+
+        //#region Data Source & Functions
+
+        var populateData = function (selectedDate) {
+            var epochDate = moment(selectedDate).unix();
+            var params = "TradeDate=" + encodeURIComponent(epochDate);
+
+            var iframeElement0 = document.getElementById("sheet1");
+            iframeElement0.src = "./DealCutOffFcyPreview?" + params + "&SheetIndex=0";
+
+            /*var iframeElement2 = document.getElementById("sheet2");
+            iframeElement2.src = "./DealCutOffFcyPreview?" + params + "&SheetIndex=1";
+
+            var iframeElement3 = document.getElementById("sheet3");
+            iframeElement3.src = "./DealCutOffFcyPreview?" + params + "&SheetIndex=2";*/
+        }
+
+        //#endregion
+
+        //#region Other Widgets
+        $dateSelectionBtn = $("#dateSelectionBtn").dxDateBox({
+            type: "date",
+            displayFormat: "dd/MM/yyyy",
+            value: new Date(),
+            onValueChanged: function (data) {
+                populateData(data.value);
+            }
+        }).dxDateBox("instance");
+
+        $printBtn = $("#printBtn").dxDropDownButton({
+            text: "Print",
+            icon: "print",
+            type: "normal",
+            stylingMode: "contained",
+            dropDownOptions: {
+                width: 230
+            },
+            displayExpr: "name",
+            keyExpr: "id",
+            items: [
+                { id: 1, name: "Excel Workbook (*.xlsx)", icon: "fa fa-file-excel-o" },
+                { id: 2, name: "PDF", icon: "fa fa-file-pdf-o" }
+            ],
+            onItemClick: function (e) {
+                app.toast("Generating...");
+
+                var data = {
+                    TradeDate: moment($dateSelectionBtn.option("value")).unix(),
+                    isExportAsExcel: (e.itemData.id == 1)
+                };
+
+                $.ajax({
+                    type: "POST",
+                    url: referenceUrl.printRequest,
+                    data: data,
+                    dataType: "text",
+                    success: function (data) {
+                        var url = referenceUrl.printResponse + data;
+                        window.location = url;
+                    },
+                    fail: function (jqXHR, textStatus, errorThrown) {
+                        app.alertError(textStatus + ": " + errorThrown);
+                    },
+                    complete: function (data) {
+
+                    }
+                });
+
+                e.event.preventDefault();
+            }
+        }).dxDropDownButton("instance");
+
+        var tabPanel = $("#tabpanel-container").dxTabPanel({
+            dataSource: [
+                { title: "Summary"}
+            ],
+            deferRendering: false,
+            //itemTitleTemplate: $("#dxPanelTitle"),
+            showNavButtons: true
+        });
+
+        //#endregion
+
+        // #region DataGrid
+
+
+        // #endregion DataGrid
+
+        //#region Events
+
+
+
+        //#endregion
+
+        //#region Immediate Invocation function
+
+
+        populateData(new Date());
+
+        //#endregion
+    });
+}(window.jQuery, window, document));
