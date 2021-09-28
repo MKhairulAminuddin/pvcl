ad11c257714e126e2aae66a8ac4c1e3a4d9927d0
khaliq <khaliq2network@gmail.com>
2021-09-28 15:57:01 +0800

    fix resubmit logic

--- Presentation/xDC.Web/Controllers/FidController.cs
+++ Presentation/xDC.Web/Controllers/FidController.cs
@@ -124,7 +124,7 @@ namespace xDC_Web.Controllers
                             ApprovedDate = form.ApprovedDate,
 
                             EnableSubmitForApproval = (form.FormStatus != Common.FormStatus.PendingApproval && form.ApprovedBy == null),
-                            EnableResubmitBtn = (form.FormStatus == Common.FormStatus.Approved || form.FormStatus == Common.FormStatus.Rejected && form.ApprovedBy == null),
+                            EnableResubmitBtn = (form.FormStatus == Common.FormStatus.Approved || form.FormStatus == Common.FormStatus.Rejected),
                             EnableSaveAsDraftBtn = (form.FormStatus == Common.FormStatus.Draft && form.ApprovedBy == null),
                             EnableReassign = (form.FormStatus == Common.FormStatus.PendingApproval && form.ApprovedBy != User.Identity.Name)
                         };
