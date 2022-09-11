2ca395a83e81afac55cc09f4ccf3da2be527a871
khaliq <khaliq2network@gmail.com>
2022-09-12 07:34:09 +0800

    WIP

--- Presentation/xDC.Web/Scripts/app/setting/notificationConfig.js
+++ Presentation/xDC.Web/Scripts/app/setting/notificationConfig.js
@@ -36,42 +36,123 @@
 
         });
 
-        $("#cnEmail").dxTagBox({
-            dataSource: adUsers,
-            displayExpr: "displayName",
-            valueExpr: "email",
-            searchEnabled: true,
-            itemTemplate: function (data) {
-                return "<div class='active-directory-dropdown'>" +
-                    "<p class='active-directory-title'>" + data.displayName + "</p>" +
-                    "<p class='active-directory-subtitle'>" + data.title + ", " + data.department + "</p>" +
-                    "<p class='active-directory-subtitle'>" + data.email + "</p>" +
-                    "</div>";
-            }
-        }).dxTagBox("instance");
+        $("#cnEmail").dxTextBox({
+            validationRules: [{
+                type: "required",
+                message: "fak u!"
+            }]
+        });
+
+        var cnEmailEnable = $("#cnEmailEnable").dxSwitch('instance');
+        var cnEmailCcEnable = $("#cnEmailCcEnable").dxSwitch('instance');
+        var peEmailEnable = $("#peEmailEnable").dxSwitch('instance');
+        var peEmailCcEnable = $("#peEmailCcEnable").dxSwitch('instance');
+        var propertyEmailEnable = $("#propertyEmailEnable").dxSwitch('instance');
+        var propertyEmailCcEnable = $("#propertyEmailCcEnable").dxSwitch('instance');
+        var loanEmailEnable = $("#loanEmailEnable").dxSwitch('instance');
+        var loanEmailCcEnable = $("#loanEmailCcEnable").dxSwitch('instance');
+        var fcaTaggingEmailEnable = $("#fcaTaggingEmailEnable").dxSwitch('instance');
+        var approvedTreasuryEnable = $("#approvedTreasuryEnable").dxSwitch('instance');
+
+        var cnEmail = $("#cnEmail").dxTextBox({}).dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value);}
+            }],
+        }).dxTextBox("instance");
+        var cnEmailCc = $("#cnEmailCc").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var peEmail = $("#peEmail").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var peEmailCc = $("#peEmailCc").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var propertyEmail = $("#propertyEmail").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var propertyEmailCc = $("#propertyEmailCc").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var loanEmail = $("#loanEmail").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var loanEmailCc = $("#loanEmailCc").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var fcaTaggingEmail = $("#fcaTaggingEmail").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
+        var approvedTreasury = $("#approvedTreasury").dxValidator({
+            validationRules: [{
+                type: 'custom',
+                message: 'Invalid input',
+                validationCallback(params) { return validateEmails(params.value); }
+            }],
+        }).dxTextBox("instance");
 
         $("#tradeSettlementSaveButton").dxButton("instance").option("onClick", function (e) {
             
-            var selectedCnEmail = $("#cnEmail").dxTagBox("instance").option('selectedItems');
-            var cnEmail = [];
-            $.each(selectedCnEmail, function (key, value) {
-                cnEmail.push(value.email);
-            });
-
             var data = {
-                "tsCnEmail": cnEmail,
-                "tsCnEmailCc": cnEmailCc,
-                "tsPeEmail": peEmail,
-                "tsPeEmailCc": peEmailCc,
-                "tsPropertyEmail": propertyEmail,
-                "tsPropertyEmailCc": propertyEmailCc,
-                "tsLoanEmail": loanEmail,
-                "tsLoanEmailCc": loanEmailCc,
-                "tsFcaTaggingEmail": fcaTaggingEmail,
-                "tsApprovedTreasury": approvedTreasury
-            }
+                "tsCnEmailEnable": cnEmailEnable.option('value'),
+                "tsCnEmailCcEnable": cnEmailCcEnable.option('value'),
+                "tsPeEmailEnable": peEmailEnable.option('value'),
+                "tsPeEmailCcEnable": peEmailCcEnable.option('value'),
+                "tsPropertyEmailEnable": propertyEmailEnable.option('value'),
+                "tsPropertyEmailCcEnable": propertyEmailCcEnable.option('value'),
+                "tsLoanEmailEnable": loanEmailEnable.option('value'),
+                "tsLoanEmailCcEnable": loanEmailCcEnable.option('value'),
+                "tsFcaTaggingEmailEnable": fcaTaggingEmailEnable.option('value'),
+                "tsApprovedTreasuryEnable": approvedTreasuryEnable.option('value'),
 
-            $.ajax({
+                "tsCnEmail": cnEmail.option('value'),
+                "tsCnEmailCc": cnEmailCc.option('value'),
+                "tsPeEmail": peEmail.option('value'),
+                "tsPeEmailCc": peEmailCc.option('value'),
+                "tsPropertyEmail": propertyEmail.option('value'),
+                "tsPropertyEmailCc": propertyEmailCc.option('value'),
+                "tsLoanEmail": loanEmail.option('value'),
+                "tsLoanEmailCc": loanEmailCc.option('value'),
+                "tsFcaTaggingEmail": fcaTaggingEmail.option('value'),
+                "tsApprovedTreasury": approvedTreasury.option('value')
+            }
+            console.log(data);
+
+            /*$.ajax({
                 data: data,
                 dataType: 'json',
                 url: '../api/setting/UpdateTsNotificationSetting',
@@ -81,9 +162,20 @@
 
             }).fail(function (jqXHR, textStatus, errorThrown) {
                 app.alertError(textStatus + ': ' + errorThrown);
-            });
+            });*/
 
         });
+
+        function validateEmails(string) {
+            var regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            var result = string.replace(/\s/g, "").split(/,/);
+            for (var i = 0; i < result.length; i++) {
+                if (!regex.test(result[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
 
     });
 }(window.jQuery, window, document));
